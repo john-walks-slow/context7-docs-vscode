bd14541ef256f21a809bede5f69a724eb19633ac
JohnnRen <john.ren.contact@gmail.com>
2026-03-16 01:14:23 +0800

    chore: remove unused language library entries

--- src/constants/libraries.ts
+++ src/constants/libraries.ts
@@ -59,7 +59,7 @@ export const COMMON_LIBRARIES: LibraryItem[] = [
     name: 'playwright',
     description: 'End-to-end testing',
   },
-  
+
   // === Python ===
   {
     id: '/pallets/flask',
@@ -86,133 +86,7 @@ export const COMMON_LIBRARIES: LibraryItem[] = [
     name: 'sqlalchemy',
     description: 'Python SQL toolkit and ORM',
   },
-  
-  // === Rust ===
-  {
-    id: '/tokio-rs/tokio',
-    name: 'tokio',
-    description: 'Rust async runtime',
-  },
-  {
-    id: '/serde-rs/serde',
-    name: 'serde',
-    description: 'Serialization framework for Rust',
-  },
-  {
-    id: '/actix/actix-web',
-    name: 'actix-web',
-    description: 'Powerful, pragmatic web framework for Rust',
-  },
-  {
-    id: '/diesel-rs/diesel',
-    name: 'diesel',
-    description: 'Safe, extensible ORM for Rust',
-  },
-  
-  // === Go ===
-  {
-    id: '/gin-gonic/gin',
-    name: 'gin',
-    description: 'HTTP web framework for Go',
-  },
-  {
-    id: '/go-gorm/gorm',
-    name: 'gorm',
-    description: 'ORM library for Go',
-  },
-  {
-    id: '/go-chi/chi',
-    name: 'chi',
-    description: 'Lightweight, idiomatic router for Go',
-  },
-  
-  // === Java/Kotlin ===
-  {
-    id: '/spring-projects/spring-framework',
-    name: 'spring',
-    description: 'Java application framework',
-  },
-  {
-    id: '/JetBrains/kotlin',
-    name: 'kotlin',
-    description: 'Modern programming language',
-  },
-  {
-    id: '/square/retrofit',
-    name: 'retrofit',
-    description: 'Type-safe HTTP client for Android and Java',
-  },
-  
-  // === C# ===
-  {
-    id: '/dotnet/aspnetcore',
-    name: 'aspnetcore',
-    description: 'Cross-platform web framework for .NET',
-  },
-  {
-    id: '/JamesNK/Newtonsoft.Json',
-    name: 'newtonsoft',
-    description: 'Json.NET for .NET',
-  },
-  {
-    id: '/AutoMapper/AutoMapper',
-    name: 'automapper',
-    description: 'Object-object mapper for .NET',
-  },
-  
-  // === Dart/Flutter ===
-  {
-    id: '/flutter/flutter',
-    name: 'flutter',
-    description: 'Build apps for any screen',
-  },
-  {
-    id: '/rrousselGit/riverpod',
-    name: 'riverpod',
-    description: 'Reactive state management for Flutter',
-  },
-  {
-    id: '/jonataslaw/getx',
-    name: 'get',
-    description: 'Flutter state management, navigation',
-  },
-  
-  // === Ruby ===
-  {
-    id: '/rails/rails',
-    name: 'rails',
-    description: 'Ruby on Rails web framework',
-  },
-  {
-    id: '/sinatra/sinatra',
-    name: 'sinatra',
-    description: 'Ruby micro web framework',
-  },
-  
-  // === PHP ===
-  {
-    id: '/laravel/framework',
-    name: 'laravel',
-    description: 'PHP web framework',
-  },
-  {
-    id: '/symfony/symfony',
-    name: 'symfony',
-    description: 'PHP framework',
-  },
-  
-  // === Swift ===
-  {
-    id: '/Alamofire/Alamofire',
-    name: 'alamofire',
-    description: 'HTTP networking library for Swift',
-  },
-  {
-    id: '/onevcat/Kingfisher',
-    name: 'kingfisher',
-    description: 'Image loading library for Swift',
-  },
-  
+
   // === CSS ===
   {
     id: '/twbs/bootstrap',
